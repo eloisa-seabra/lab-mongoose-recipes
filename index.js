982ac2cce792c06447b1c4bcc401871aab548b7b
Eloisa Seabra <seabra.eloisa@gmail.com>
2020-07-23 19:40:13 +0100

    couldnt get title of each recipe in iteration 3

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: true
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -20,7 +21,40 @@ mongoose
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'Burrata',
+      level: 'Easy Peasy',
+      ingredients: ['tomato', 'buffalo mozzarella'],
+      cuisine: 'Italian',
+      dishType: 'snack',
+      image:
+        'https://www.google.com/search?q=burrata&rlz=1C5CHFA_enPT905PT905&sxsrf=ALeKk01O6EopXDcqGEo2s_k33gt28tikxw:1595525272292&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiv4tbo8uPqAhVT6uAKHe1fBkIQ_AUoAXoECBsQAw&biw=1598&bih=883#imgrc=pLyrj3bS6mSShM',
+      duration: 15,
+      creator: 'Lorenzo Bianchin',
+      created: new Date(1900, 9, 7)
+    });
+  })
+  .then(recipe => {
+    console.log('Created recipe', recipe.title);
+    return Recipe.insertMany(data);
+  })
+  .then(data => {
+    console.log('Created multiple recipes', data);
+    return Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 100 }
+    );
+  })
+  .then(data => {
+    console.log('Sucess! Updated recipe!', data);
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(data => {
+    console.log('Deleted recipe!', data);
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('disconnected from mongoDB');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
